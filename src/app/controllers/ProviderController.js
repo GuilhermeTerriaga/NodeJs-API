78674cf1b903af232e7c22554390b773f59b2a50
guilherme terriaga <guilherme.terriaga@gmail.com>
2021-01-14 22:24:45 -0300

    added URL Virtual field to Models/File'

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -12,7 +12,7 @@ class ProviderController {
         {
           model: File,
           as: 'avatar',
-          attributes: ['name', 'path'],
+          attributes: ['name', 'path', 'url'],
         },
       ],
     });
